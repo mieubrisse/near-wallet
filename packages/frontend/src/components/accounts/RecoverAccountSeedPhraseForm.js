4c1ed0bd2751e7ace4312c9e43607832d38b622a
Osman Abdelnasir <esaminu1@icloud.com>
2021-09-20 21:32:12 +0400

    ADD testId and move constant

--- packages/frontend/src/components/accounts/RecoverAccountSeedPhraseForm.js
+++ packages/frontend/src/components/accounts/RecoverAccountSeedPhraseForm.js
@@ -34,6 +34,7 @@ const RecoverAccountSeedPhraseForm = ({
                 disabled={!isLegit || mainLoader}
                 sending={actionsPending(['RECOVER_ACCOUNT_SEED_PHRASE', 'REFRESH_ACCOUNT_OWNER'])}
                 sendingString='button.recovering'
+                data-test-id="seedPhraseRecoverySubmitButton"
             >
                 <Translate id='button.findMyAccount' />
             </FormButton>
